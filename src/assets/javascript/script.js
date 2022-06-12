efafb295e5ecd5721103931a2133d796bba4df84
José <senhor.jotaa@gmail.com>
2022-06-12 18:28:54 -0400

    Starny - Your destiny lays in the stars add: background, blur, favicon, title, images dir del: useless code lines fix: minor issues, main css file

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -1,23 +1,28 @@
 const inputQuestion = document.querySelector('#inputQuestion');
 const answerElement = document.querySelector('#answer');
 const modal = document.getElementById('modal');
+const container = document.getElementById('container');
+document.onkeydown = onKeyDown;
 
 const answer = [
+  'No.',
+  'Yes.',
+  'Perhaps.',
   'For sure!',
-  `I'm not sure.`,
-  `Don't count on that.`,
   'No doubt!',
+  'Probably.',
+  'I think so.',
+  `I'm not sure.`,
   'Ask again later.',
   'Yes, definitely!',
+  'Yes, eventually.',
   'My response is no.',
+  `Don't count on that.`,
+  'Focus and ask again.',
   'You can count on that.',
   'Better not tell you yet.',
-  'I think so.',
   'My sources tell me not so.',
-  'Probably.',
-  `It's not possible to predict it now.`,
-  'Yes.',
-  'Focus and ask again.'
+  `It's not possible to predict it now.`
 ];
 
 function response() {
@@ -29,9 +34,25 @@ function response() {
     answerElement.innerHTML = answer[randomNumber];
 
     modal.classList.add('open');
+
+    container.classList.add('blur');
   }
 }
 
 function closeModal() {
   modal.classList.remove('open');
+  container.classList.remove('blur');
+}
+
+function onKeyDown() {
+  const event = window.event;
+  const input = document.getElementById('inputQuestion');
+
+  if (event.keyCode === 27) {
+    closeModal();
+  }
+
+  if (event.keyCode === 13) {
+    response();
+  }
 }
